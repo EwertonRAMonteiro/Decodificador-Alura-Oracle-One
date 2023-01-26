f61a53376ed445136870dbddc9ec0001b635c962
EwertonRAMonteiro <ewertonra.monteiro@gmail.com>
2023-01-26 19:24:09 -0300

    logica js e ajustes no html

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,34 +1,29 @@
-var textIn = document.querySelector('#bnt-incriptar');
-var textOut = document.querySelector('#bnt-descriptar');
+var textInput = document.querySelector('#textIn');
+var textOut = document.querySelector('#outText');
 
-function encriptar() {
-    var texto = textIn.valeu;
+function encriptar(){
+    var texto = textInput.value;
 
-    var resultCripto = texto.replace(/e/g, "enter")
-                            .replace(/i/g, "imes")
-                            .replace(/a/g, "ai")
-                            .replace(/o/g, "ober")
-                            .replace(/u/g, "ufat");
+    var resultCripto = texto.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat")
 
-    document.getElementById('bnt-descriptar').innerHTML = '<textarea readonly id="bnt-incriptar">'
-        + resultCripto +
-        + '<textarea>' + '<button class="bnt-copiar" id="copiar" onclick="copiar()">Copiar<button>'
+    document.getElementById('outText').innerHTML = '<textarea readonly id="textIn">'
+        + resultCripto
+        + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar<button>'
 }
 
 function descriptar() {
-    var texto = textIn.valeu;
+    var texto = textInput.value;
 
-    var resultDesripto = texto.replace(/enter/g, "e")
-                              .replace(/imes/g, "i")
-                              .replace(/ai/g, "a")
-                              .replace(/ober/g, "o")
-                              .replace(/ufat/g, "u");
+    var resultDesripto = texto.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u")
 
-    document.getElementById('bnt-descriptar').innerHTML = '<textarea readonly id="bnt-incriptar">'
-        + resultDesripto +
-        + '<textarea>' + '<button class="bnt-copiar" id="copiar" onclick="copiar()">Copiar<button>'
+    document.getElementById('outText').innerHTML = '<textarea readonly id="textIn">'
+        + resultDesripto + '</textarea>' + '<button class="btn-copiar" id="copiar" onclick="copiar()">Copiar<button>'
 }
 
 function copiar(){
-    var textoCopiar = document.getElementById()
+    var textoCopiar = document.getElementById('textInput');
+
+    textoCopiar.select();
+    document.execCommand('copy');
+    alert('Texto Copiado');
 }
